44f6ac92b4d17d15116c0b45010ee45ac9f231e5
Israel Fonseca <israel.bgf@gmail.com>
2015-02-28 13:17:32 -0300

    fixed import and assert

--- src/test/module.js
+++ src/test/module.js
@@ -1,10 +1,10 @@
-import helloworld from "../core/module"
+import helloworld from "../usecases/module"
 import chai from 'chai'
 
 chai.should();
 
 describe('Hello world usecase', () => {
     it('should return a fancy message', () => {
-        helloworld().should.be.equal('Hello All Mighty ES6!')
+        helloworld().should.be.equal('Hello All Mighty ES6!!')
     })
 })
